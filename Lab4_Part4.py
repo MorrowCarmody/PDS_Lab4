6b68a416b57c52def21c4d239bc99fc235689e63
MorrowCarmody <morrow.carmody@gmail.com>
2023-06-22 14:33:53 -0500

    add error handling, begin logic

--- Lab4_Part4.py
+++ Lab4_Part4.py
@@ -1,3 +1,37 @@
 # Matt Carmody
 # COSC 6375
 # Lab 4 Part 4
+
+def Tower(n, start, helper, end):
+    """Move n disks from the start pin to the end pin."""
+    if(type(n) != int):
+        # non-integer value: print error message
+        print('The input must be an integer.')
+    elif(n < 0):
+        # negative value: print error message
+        print('The sequence of a negative value is not possible.')
+    else:
+        print(f'n: {n}, start: {start}, helper: {helper}, end: {end}.')
+        return
+
+Tower(3, 'A', 'B', 'C')
+
+"""
+> 1 DISK
+Move disk 1 from START to END
+
+> 2 DISKS
+Move disk 1 from START to HELPER
+Move disk 2 from START to END
+Move disk 1 from HELPER to END
+
+> 3 DISKS
+Move disk 1 from START to END
+Move disk 2 from START to HELPER
+Move disk 1 from END to HELPER
+Move disk 3 from START to END
+Move disk 1 from HELPER to START
+Move disk 2 from HELPER to END
+Move disk 1 from START to END
+
+"""
